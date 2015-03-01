6f546ffad7727fce207a67f3e8341713849fc096
Aidan O'Grady <theaidanogrady@gmail.com>
2015-03-01 19:42:47 +0000

    Added the other stuff of the first PDF

--- src/server/WebServer.java
+++ src/server/WebServer.java
@@ -33,7 +33,7 @@ public final class WebServer {
 			
 			while(true){
 				Socket client = socket.accept(); // Connection found
-				System.out.println("Connection found");
+				System.out.println("\nConnection found");
 				HttpRequest request = new HttpRequest(client);
 				Thread thread = new Thread(request);
 				thread.start();
@@ -92,17 +92,77 @@ final class HttpRequest implements Runnable{
 		BufferedReader br = new BufferedReader(isr);
 		
 		String requestLine = br.readLine();
-		System.out.println();
+		System.out.println("\nRequest Line:\n========");
 		System.out.println(requestLine);
 		
+		System.out.println("\nHeader Lines:\n========");
 		String headerLine = br.readLine();
 		while(headerLine != null){
 			System.out.println(headerLine);
 			headerLine = br.readLine();
+		}		
+		// Extract filename from the request line
+		StringTokenizer tokens = new StringTokenizer(requestLine);
+		tokens.nextToken(); // skip method
+		String fileName = tokens.nextToken();
+		
+		fileName = "." + fileName;
+		
+		FileInputStream fis = null;
+		boolean fileExists = true;
+		try{
+			fis = new FileInputStream(fileName);
+		} catch(FileNotFoundException e){
+			fileExists = false;
+		}
+		
+		String statusLine = null;
+		String contentTypeLine = null;
+		String entityBody = null;
+		if(fileExists){
+			statusLine = "HTTP/1.1 200 OK";
+			contentTypeLine = "Content-type: " + contentType(fileName) + CRLF;
+		} else{
+			statusLine = "HTTP/1.1 404 Not Found";
+			contentTypeLine = "text/html";
+			entityBody = "<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+					"<BODY>Not Found</BODY></HTML>";
+		}
+
+		
+		System.out.println("\nResponse:\n========");
+		System.out.println(statusLine);
+		System.out.println(contentTypeLine);
+		os.writeBytes(statusLine);
+		os.writeBytes(contentTypeLine);
+		os.writeBytes(CRLF);
+		
+		if(fileExists){
+			sendBytes(fis, os);
+			fis.close();
+		}else{
+			os.writeBytes(entityBody);
 		}
 		os.close();
 		br.close();
 		socket.close();
 	}
-	
+		
+	private void sendBytes(FileInputStream fis, DataOutputStream os) throws
+	Exception {
+		byte[] buffer = new byte[1024];
+		int bytes = 0;
+		
+		while((bytes = fis.read(buffer)) != -1){
+			os.write(buffer, 0, bytes);
+		}
+		
+	}
+
+	private String contentType(String fileName){
+		if(fileName.endsWith(".htm") || fileName.endsWith(".html")){
+			return "text/html";
+		}
+		return "application/octet-stream";
+	}
 }
